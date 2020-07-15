e67cab0979cc22ebb95cd355715efe1a75b4db4c
Naeem009 <56760480+Naeem009@users.noreply.github.com>
2020-07-15 21:42:55 +0500

    Update Home.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -8,7 +8,8 @@ function Home() {
       <h5>An online store to facilitae customers. We offer elegant styling for men, ladies and children.
         Different varities, occassional, seasonal and functional shoes are available for our valued customers at affordable price.
         <br/><br/>Secure payment method.<br/><br/>
-        Attractive Designs.
+        Attractive Designs.<br/><br/>
+        Easy Return Policy
 
       </h5>
 
